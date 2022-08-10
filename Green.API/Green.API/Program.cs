d998f17653bcc6d31ff139d8ad8680a071be5022
Brandon <figueredo407@gmail.com>
2022-08-10 14:52:32 -0400

    Update Program.cs

--- Green.API/Green.API/Program.cs
+++ Green.API/Green.API/Program.cs
@@ -18,7 +18,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(p => { p.RoutePrefix = String.Empty; });
 }
 
 app.UseHttpsRedirection();
