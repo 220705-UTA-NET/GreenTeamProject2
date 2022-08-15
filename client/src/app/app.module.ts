fc923ba98a7f0ee0e8df13e56123ccce80fee13c
Dbeidels <dbeidels@macalester.edu>
2022-08-15 18:43:07 -0500

    starting paginator

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { PaginatorComponent } from './paginator/paginator.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    PaginatorComponent
+    
   ],
   imports: [
     BrowserModule,
