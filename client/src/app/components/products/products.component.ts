593b223ace3c66430c2dae227226b97f77ea3430
Brandredo <figueredo407@gmail.com>
2022-08-16 15:07:29 -0600

    component update

---
+++ client/src/app/components/products/products.component.ts
@@ -0,0 +1,57 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { MockProducts } from 'src/app/MockProducts';
+import { Product } from 'src/app/Product';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.scss']
+})
+
+export class ProductsComponent implements OnInit {
+
+  // fetch the products of the selected category from the api/database and set it equal to products (replace MockProducts)
+  products: Product[] = MockProducts;
+
+  category: string = "";
+
+  constructor(private route: ActivatedRoute, private http: HttpClient) { }
+
+  ngOnInit(): void {
+  
+    // set the category of the current route
+    this.category = this.route.snapshot.params['category'];
+
+    // subscribe to callback so that if params is updated then the component view is updated
+    // this.route.params.subscribe( (params: Params) => { this.category = params['category'] });
+
+    switch(this.category) {
+
+      case "all":
+        // get all products of this category
+        // this.products = result from http get request
+        break;
+      case "music":
+
+        break;
+      case "nft":
+
+        break;
+      case "document":
+
+        break;
+      case "videogame":
+
+        break;
+      case "audiobook":
+
+        break;
+      
+    }
+    
+  
+  }
+
+}
