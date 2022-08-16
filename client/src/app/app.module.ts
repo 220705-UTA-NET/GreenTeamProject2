4eb86a271ce9cd5f5fae3ab7cdbeab1dbe5adad4
Corvus <gdiaz1092@gmail.com>
2022-08-16 11:47:36 -0400

    added ripple component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,24 +1,31 @@
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { PaginatorComponent } from './paginator/paginator.component';
+import { RippleComponent } from './components/ripple/ripple.component';
+
+// Material
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatRippleModule } from '@angular/material/core';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { PaginatorComponent } from './paginator/paginator.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    PaginatorComponent
+    PaginatorComponent,
+    RippleComponent
 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatRippleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
