a4aa91721a60518c11f19fe6ada88fbbb41c6f81
Dbeidels <dbeidels@macalester.edu>
2022-08-17 15:05:16 -0500

    prepare to merge

--- client/src/app/paginator/paginator.component.ts
+++ client/src/app/paginator/paginator.component.ts
@@ -1,13 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-import {MatPaginatorModule} from '@angular/material/paginator';
+// import {MatPaginatorModule} from '@angular/material/paginator';
+
+
+
 
 @Component({
   selector: 'app-paginator',
   templateUrl: './paginator.component.html',
-  styleUrls: ['./paginator.component.scss']
+  styleUrls: ['./paginator.component.scss'],
+  
 })
+
 export class PaginatorComponent implements OnInit {
 
+  // length = 100;
+  // pageSize = 10;
+  // pageSizeOptions: number[] = [5, 10, 25, 100];
+  // matpaginator: MatPaginatorModule;
   constructor() { }
 
   ngOnInit(): void {
